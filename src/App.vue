8daa473197f7c9197a1cfb2fa816562c9100fdc6
unknown <rodrigodlq@hotmail.com>
2019-11-12 19:52:35 +0000

    Navbar with error

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+    <NavBar></NavBar>
+
+    <div  class="">
+        <router-view />
     </div>
-    <router-view />
+
+    <Footer></Footer>
+    
   </div>
 </template>
+
+<script>
+// Components
+import Navbar from './views/Navbar'
+import Footer from './views/Footer'
+
+
+export default {
+  components: {
+            Navbar,
+            Footer
+  },
+}
+</script>
 
 <style lang="scss">
     @import "./assets/main";
