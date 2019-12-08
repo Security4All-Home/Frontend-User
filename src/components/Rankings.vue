ed581d479e22bc9434c04c003ae4a48906840afc
axemm99 <inesmm1999@gmail.com>
2019-12-08 14:24:02 +0000

    changes (cards)

--- src/components/Rankings.vue
+++ src/components/Rankings.vue
@@ -1,4 +1,4 @@
-<template>
+<template id="ranking">
     <div class="rankings">
       <br>
       <br>
@@ -10,48 +10,60 @@
     <div class="container" id="tableRanking">
 
       <div class="columns">
-        <div class="column is-4 is-offset-0">
-          <input id="searchBar" class="input" type="text" placeholder="Search">
+        <div class="column is-6 is-offset-2 is-desktop">
+          <input id="searchBar"
+              icon="magnify" 
+              class="input" 
+              type="text" 
+              placeholder="Search">
+          <!--<b-autocomplete
+              v-model="searchBar"
+              placeholder="Pesquise aqui por um utilizador"
+              icon="magnify"
+              @select="option => this.selected = option"
+            ></b-autocomplete>-->
         </div>
-        <div class="column">
-          <button class="button is-primary">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-        <div class="column is-1 is-offset-5">
-          <button id="changeTable" class="button is-secondary">
+        <div class="column is-1-desktop">
+          <a id="changeTable" class="button" v-bind:style="isActive(1)" @click="view=1">
             <i class="fas fa-table"></i>
-          </button>
+          </a>
         </div>
-        <div class="column ">
-          <button id="changeCard" class="button is-primary">
-            <i class="fas fa-th-large"></i>
-          </button>
+        <div class="column is-1-desktop">
+         <!-- <router-link :to="{ name: 'rankingsCards' }">-->
+            <a id="changeCard" class="button" @click="view=2" v-bind:style="isActive(2)" >
+              <i class="fas fa-address-card"></i>
+            </a>
+         <!-- </router-link>-->
         </div>
       </div>
+      <div v-if="view==1">
         <section>
           <b-table
-              v-for="user in tempRanking" :key="user.id"
+          id="dataTable"
+              :data="data"
               ref="table"
               :mobile-cards	="false"
-              hoverable>
+              hoverable              
+            :sort-icon="arrow-up"
+            :sort-icon-size="is-small">
 
-              <template  id="table">
-                  <b-table-column field="id" label="ID" width="0.5" centered numeric sortable>
-                      {{ user.id}}
+
+              <template slot-scope="props">
+                  <b-table-column is-current-sort field="id" label="ID" width="1" centered numeric sortable>
+                      {{ props.row.id }}
                   </b-table-column>
 
                   <b-table-column field="username" width="10" label="User" centered sortable>
-                      {{ user.name }}
+                      {{ props.row.username }}
                   </b-table-column>
 
-                  <b-table-column field="photo" width="15" label="" centered sortable>
-                     <img id="imageTable" :src='user.photo'> 
+                  <b-table-column field="photo" width="20" label="" centered>
+                     <img id="imageTable" :src='props.row.photo'> 
                   </b-table-column>
 
-                  <b-table-column field="points" width="5" label="Points" numeric centered sortable>
+                  <b-table-column field="points" width="10" label="Points" numeric centered sortable>
                       <span>
-                          {{ user.points }}
+                          {{ props.row.points }}
                       </span>
                   </b-table-column>
               </template>
@@ -71,6 +83,31 @@
               </template>
           </b-table>
       </section>
+      </div>
+
+      
+      <div id="cardView" v-if="view==2" class="columns is-multiline">
+        <div class="column is-12-mobile is-6-tablet is-3-desktop"
+          v-for="(person,i) in persons"
+          :key="i">
+        <div class="card" id="cardView">
+          <div class="card-image">
+            <figure class="image">
+              <img id="imageCard" :src='person.photo' alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p  class="title is-10">{{person.id}}</p>
+                <p class="subtitle is-12">{{person.username}}</p>
+                <p class="subtitle is-20"> {{person.points}}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
     </div>
 </template>
@@ -82,57 +119,128 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
-//import
-import store from '../store/index.js'
-
-Vue.use(Buefy, store)
-
-
+Vue.use(Buefy)
 export default {
   name: "Rankings",
-  data: function() {
-    return {
-      tempRanking: []
-    };
-  },
-   /*data() {
+   data() {
             const data = [
                 { 'id': 1, 'username': 'Jessie', 'photo': 'https://contigo.uol.com.br/orinoco/media/images/large/2019/10/15/paolla-oliveira-aparece-chorando-em-foto-como-vivi-guedes-na-web-1186472.jpg', 'points': 1005 },
                 { 'id': 2, 'username': 'John', 'photo': 'https://static1.purepeople.com.br/articles/5/27/69/65/@/3147374-pedro-scooby-tem-suposta-foto-intima-e-c-950x0-2.jpg', 'points': 903 },
                 { 'id': 3, 'username': 'Tina', 'photo': 'https://conteudo.imguol.com.br/c/entretenimento/38/2019/07/30/duda-reis-e-acusada-de-mexer-no-corpo-em-foto-1564498888379_v2_450x600.png', 'points': 554 },
-                { 'id': 4, 'username': 'Aaron', 'photo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbSBvQQubxvFZ3hnf2NPLGmTZGnGRC9b4AiV3xJuNZ6YdyJc_L&s', 'points': 186 },
+                { 'id': 4, 'username': 'Aaron', 'photo': 'https://cdn-ofuxico.akamaized.net/img/upload/noticias/2019/12/07/henry_365654_36.jpg', 'points': 186 },
                 { 'id': 5, 'username': 'Anne', 'photo': 'https://static1.purepeople.com.br/articles/7/27/94/37/@/3174773-thais-fersoza-chamou-atencao-por-foto-de-950x0-3.jpg', 'points': 55 }
-            ],
-            
-              tempRanking: []
+            ]
+
+            let persons = [
+              {
+                id:1,
+                username: "Jessie",
+                photo: "https://contigo.uol.com.br/orinoco/media/images/large/2019/10/15/paolla-oliveira-aparece-chorando-em-foto-como-vivi-guedes-na-web-1186472.jpg",
+                points: 1005
+              },
+              {
+                id:2,
+                username: "John",
+                photo: "https://static1.purepeople.com.br/articles/5/27/69/65/@/3147374-pedro-scooby-tem-suposta-foto-intima-e-c-950x0-2.jpg",
+                points: 903
+              },
+              {
+                id:3,
+                username: "Tina",
+                photo: "https://conteudo.imguol.com.br/c/entretenimento/38/2019/07/30/duda-reis-e-acusada-de-mexer-no-corpo-em-foto-1564498888379_v2_450x600.png",
+                points: 554
+              },
+              {
+                id:4,
+                username: "Aaron",
+                photo: "https://cdn-ofuxico.akamaized.net/img/upload/noticias/2019/12/07/henry_365654_36.jpg",
+                points: 186
+              },
+              {
+                id:5,
+                username: "Anne",
+                photo: "https://static1.purepeople.com.br/articles/7/27/94/37/@/3174773-thais-fersoza-chamou-atencao-por-foto-de-950x0-3.jpg",
+                points: 55
+              }
+            ]
             return {
                 data,
-                tempRanking
+                searchBar: "",
+                filteredUsers: [],
+                view: 1,
+                persons
+
             }
-   },*/
+   },
   /*data: function() {
     return {
       tempRanking: []
     };
   },*/
   created() {
-    this.tempRanking = this.$store.getters.ranking;
-    //console.log(this.tempRanking.id)
-    for (let i = 0; i < this.tempRanking.length; i++) {
-    //console.log(this.tempRanking[i].id)
-      
-    }
+    this.tempRanking = JSON.parse(localStorage.getItem('ranking'));
+   // console.log(this.persons)
   },
   mounted() {
+   /* for (let i = 0; i < this.tempRanking.length; i++) {
+      //console.log(this.tempRanking[i].points)
+    //points.sort(function(a, b){return a-b});
+    //document.getElementById("demo").innerHTML = points;
+      
+    }*/
+  },
+  computed: {
+    /*filteredUserFunc() {
+      if (this.data) {  
 
-  }
+        this.filteredUsers = this.data.filter(data => {
+          return (
+            data.info.username
+              .toString()
+              .toLowerCase()
+              .normalize("NFD")
+              .replace(/[\u0300-\u036f]/g, "")
+              .indexOf(this.searchBar.toLowerCase()) >= 0
+          );
+        });
+
+        return this.data.length;
+      }
+    }*/
+  },
+   methods: {
+     isActive(n) {
+      //console.log("entrei")
+      return ((n==this.view)?{"background-color":"#F2F2F2"}:{"background-color":"#FFBF2F"})
+    }
+   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-#table {
-  width: 200px;
+#cardPoints {
+  color: black !important;
+}
+.card {
+  width: 300px;
+  height: 400px;
+  size: 1px;
+}
+.is-active {
+  color: #FFBF2F !important;
+}
+#cardView {
+  align-self: center !important;
+  align-items: center !important;
+  margin-left: 100px;
+  margin-right: 300px;
+}
+#imageCard {  
+  width:300px;
+  height:200px;
+  object-fit:cover;
+  border-radius: 5px;
 }
 /*#changeView{
   margin-left: 650px;
@@ -151,13 +259,15 @@ export default {
   object-fit:cover;
   border-radius:50%;
 }
-table{
+#dataTable{
+  width: 800px;
   margin-left: auto;
-  margin-right: auto;
+  margin-right: auto;  
+  align-self: center !important;
+  align-items: center !important;
 }
 #tableRanking{
   align-self: center !important;
   align-items: center !important;
 }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -27,7 +27,7 @@ const routes = [
     path: "/rankings",
     name: "rankings",
     component: Rankings
-  },
+  }
 ];
 
 const router = new VueRouter({
