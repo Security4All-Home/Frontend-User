917ee4e009cac385cceb6a448c9cfaee716e033a
axemm99 <inesmm1999@gmail.com>
2020-01-13 12:20:09 +0000

    changes

---
+++ src/views/Ranking.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="ranking">
+    <Rankings/>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Rankings from "@/components/Ranking.vue";
+
+export default {
+  name: "ranking",
+  components: {
+    Rankings
+  }
+};
+</script>
