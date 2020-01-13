066d256cbc0501cfa758888a2e5e944f7ec201bd
axemm99 <inesmm1999@gmail.com>
2020-01-13 12:17:30 +0000

    changes

---
+++ src/views/Achievements.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="achievements">
+    <Achievements/>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Achievements from "@/components/Achievements.vue";
+//import Navbar from "@/views/NavBar.vue";
+
+export default {
+  name: "achievements",
+  components: {
+    Achievements,
+   // Navbar
+  }
+};
+</script>
