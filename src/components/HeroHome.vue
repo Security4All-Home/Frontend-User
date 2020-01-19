e42c3d0b90b7db54e05d2788cb824dca9b9dd732
unknown <rodrigodlq@hotmail.com>
2020-01-19 12:35:44 +0000

    button hero home

--- src/components/HeroHome.vue
+++ src/components/HeroHome.vue
@@ -8,7 +8,9 @@
       <div class="container has-text-centered">
         <div class="column is-6 is-offset-3">
           <h1 class="title has-text-white glitch" data-text="Are you secure?">Are you secure?</h1>
+          
         </div>
+        <div class="column is-2 is-offset-5" style="margin-top:50px"> <button class="button isPrimaryBGColor is-fullwidth" style="color:#0A1F3C"><router-link :to="{name: 'security'}"><b>Find out more</b></router-link></button></div>
       </div>
     </div>
     
