09594a9d19dbc3b2d857888d7fc1ddce32e3c7b2
axemm99 <inesmm1999@gmail.com>
2020-01-21 14:31:12 +0000

    changes

--- src/API/apiUser.js
+++ src/API/apiUser.js
@@ -4,6 +4,10 @@ export function getAllUsers() {
     return get("/user")
 }
 
+export function getAllUsersSensors(id) {
+    return get(`/user/sensors/${id}`)
+}
+
 export function editUser(body) {
     return put("/user", body)
 }
